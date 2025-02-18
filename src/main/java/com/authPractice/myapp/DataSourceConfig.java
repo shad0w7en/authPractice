49dea4f03cf257619463f12149b2b37241a42826
shad0w7en <ankuryadav93932@gmail.com>
2025-02-18 20:58:35 +0000

    migrated to postgreSQL

---
+++ src/main/java/com/authPractice/myapp/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.authPractice.myapp;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(){
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        ResourceDatabasePopulator populate = new ResourceDatabasePopulator();
+        populate.addScript(new ClassPathResource("schema.sql"));
+        initializer.setDatabasePopulator(populate);
+        return initializer;
+    }
+}
